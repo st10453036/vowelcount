8fccab4b2f87a99ee9f504b8794fd08706eb59d8
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-10-16 19:03:19 +0000

    1st attempt

---
+++ src/main/java/com/mycompany/ice10/Ice10.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice10;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Ice10 {
+
+    public static void main(String[] args) {
+
+       
+      
+    }
+}
---
+++ src/main/java/com/mycompany/vowels.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class vowels {
+        public static int countVowels(String sentence) {
+        // Convert the sentence to lowercase to handle both upper and lowercase vowels
+        sentence = sentence.toLowerCase();
+int vowelCount = 0;
+        }
+
+}
